bdb95e7181b40ce40e95d506aa0594bd561f722f
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2025-09-01 00:17:52 +0530

    lab3 almost done ig

---
+++ sims/sim3/codes/bjt_ce.py
@@ -0,0 +1,8 @@
+Ic = 3.65e-3
+Vt = 25.875e-3
+eta = 1
+gm = Ic/(eta*Vt)
+ro = 1000
+
+print(gm*ro)
+
--- sims/sim3/codes/mos_cg.py
+++ sims/sim3/codes/mos_cg.py
@@ -1,3 +1,12 @@
-rs = 
-rl = 
-gm = 
+import numpy as np
+
+kp = 76.2
+Id =  6.107e-3
+ro = 1000
+rl  = 1000
+reff =  ro * rl / (ro + rl)
+vgs = 8.8926 - 6.107
+vto = 1.38
+gm = 2 * Id / (vgs - vto)
+
+print(gm * reff)
