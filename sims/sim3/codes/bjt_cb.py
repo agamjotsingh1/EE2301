371dca8d708933307add88c0242bb3f9c41391f9
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2025-09-02 14:38:18 +0530

    lab3 istgGGG

--- sims/sim3/codes/bjt_cb.py
+++ sims/sim3/codes/bjt_cb.py
@@ -5,4 +5,7 @@ Vt = 25.875e-3
 Rdashe = Vt/Ie
 Reff = Rdashe*Re/(Re + Rdashe)
 
+Vt = 25.875e-3
+Rin_base = beta*Vt/Ie
+
 print(Rc/Reff)
--- sims/sim3/codes/bjt_ce.py
+++ sims/sim3/codes/bjt_ce.py
@@ -1,22 +1,5 @@
-Ic = 3.65e-3
+Ie = 1.38e-3
 Vt = 25.875e-3
-eta = 1
-gm = Ic/(eta*Vt)
-ro = 1000
 
-Vb = 3.21
-Ve = 2.516
-Ie = 4.49e-3
-Ib = 21.74e-6
-
-beta = Ic/Ib
-RL = 470
-RE = 470
-R1 = 18e+3
-R2 = 51e+3
-
-Re = RE*RL/(RE + RL)
-Rin = 1/(1/R1 + 1/R2 + 1/(beta*Re))
-
-print(gm*ro)
+Rin = Vt/Ie
 print(Rin)
