57cdfa7261407ef7d1b1fe4c186c75b66e24b655
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2026-01-17 16:21:56 +0530

    backup

--- sims/sim3/codes/bjt_cb.py
+++ sims/sim3/codes/bjt_cb.py
@@ -3,9 +3,12 @@ Rc = 2200
 Re = 1000
 Vt = 25.875e-3
 Rdashe = Vt/Ie
-Reff = Rdashe*Re/(Re + Rdashe)
+# Reff = Rdashe*Re/(Re + Rdashe)
+#
+# Vt = 25.875e-3
+# Rdashe = Vt/Ie
+#
+# Rin_base = Vt/Ie
 
-Vt = 25.875e-3
-Rin_base = beta*Vt/Ie
-
-print(Rc/Reff)
+# print(Rc/Reff)
+print(Rdashe )
--- sims/sim3/codes/bjt_cc.py
+++ sims/sim3/codes/bjt_cc.py
@@ -7,6 +7,6 @@ R1 = 18e+3
 R2 = 51e+3
 
 Re = RE*RL/(RE + RL)
-Rin = 1/(1/R1 + 1/R2 + 1/(beta*Re))
+Rin = 1/(1/R1 + 1/R2 + 1/(beta*beta*Re))
 
 print(Rin)
